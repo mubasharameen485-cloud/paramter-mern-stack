3020cc4e69081e71ad29d6736c1e240dd44e1669
mubasharameen485-cloud <mubasharameen485@gmail.com>
2026-08-13 08:45:19 +0500

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import productRoutes from "./paramter/route.js"; // .js extension zaroori hai ES Modules me
+import cors from "cors"; // 👈 Ye import karna zaroori hai
+import productRoutes from "./paramter/route.js"; 
 
 dotenv.config();
 const app = express();
+
+// ==========================================
+// MIDDLEWARES (CORS fix)
+// ==========================================
+app.use(cors()); // 👈 Ye line Browser ko ijazat deti hai data allow karne ki
 app.use(express.json());
 
+// ==========================================
+// DATABASE CONNECTION
+// ==========================================
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(" MongoDB Connected"))
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(" DB Error", err));
 
-// Routes
+// ==========================================
+// ROUTES
+// ==========================================
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
